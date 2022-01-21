74a8b416f3624d34d2833a91c30fb44e4117d960
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2022-01-21 10:44:44 +0100

    Export models to file with pickle.

--- models/main.py
+++ models/main.py
@@ -1,8 +1,6 @@
 import string
 
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 from gensim.models import KeyedVectors
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -10,11 +8,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from stempel import StempelStemmer
 from stop_words import get_stop_words
+import pickle
 
 PREDICTED_EMOTION = 'formal'
 
 print("Started loading word2vec...")
-word2vec = KeyedVectors.load_word2vec_format("cc.pl.300.vec", binary=False)
+word2vec = KeyedVectors.load_word2vec_format("../cc.pl.300.vec", binary=False)
 print(word2vec)
 
 stemmer = StempelStemmer.polimorf()
@@ -32,16 +31,14 @@ for index, text in texts.iterrows():
     text['text'] = text['text'].translate(str.maketrans('', '', string.punctuation))
 
     words = text['text'].split(' ')
-    stemmed_words = []
+    prepared_words = []
 
     for word in words:
         if len(word) > 1 and word and word not in stop_words:
-            stemmed_word = stemmer.stem(word)
-            if stemmed_word is not None and stemmed_word not in stop_words:
-                stemmed_words.append(stemmed_word)
+            prepared_words.append(word)
 
     word_vectors = pd.DataFrame()
-    for word in stemmed_words:
+    for word in prepared_words:
         try:
             word_vec = word2vec[word]
             word_vectors = word_vectors.append(pd.Series(word_vec), ignore_index=True)
@@ -49,7 +46,6 @@ for index, text in texts.iterrows():
             pass
 
     texts_vectors = texts_vectors.append(word_vectors.mean(), ignore_index=True)
-
 
 texts_vectors[PREDICTED_EMOTION + '_class'] = texts[PREDICTED_EMOTION].multiply(2.0).round()
 print(texts_vectors)
@@ -59,7 +55,7 @@ train_x, test_x, train_y, test_y = train_test_split(texts_vectors.drop(PREDICTED
                                                     test_size=0.1)
 print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
 
-ada = AdaBoostClassifier(n_estimators=100)
+ada = AdaBoostClassifier(n_estimators=1000)
 decision_tree = DecisionTreeClassifier(min_samples_split=5, min_samples_leaf=2)
 random_forest = RandomForestClassifier(min_samples_split=5, min_samples_leaf=2)
 
@@ -76,16 +72,14 @@ for model, name in zip([ada, random_forest, decision_tree],
     print(confusion_matrix(train_y, train_pred))
 
     print("Test ACC: " + str(accuracy_score(test_y, test_pred)))
-    print(confusion_matrix(test_y, test_pred))
-
     cmt = confusion_matrix(test_y, test_pred)
     print(cmt)
 
-    plt.subplots(figsize=(8, 6))
-    plot = sns.heatmap(cmt, annot=True, cmap='Blues', fmt='g')
-    plot.set_title('Macierz pomyłek - szczęśie')
-    plot.set_xlabel('Przewidziany poziom szczęśia')
-    plot.set_ylabel('Prawdziwy poziom szczęścia')
-    plot.xaxis.set_ticklabels(['brak szczęścia', 'niewiele szczęścia', 'dużo szczęścia'])
-    plot.yaxis.set_ticklabels(['brak szczęścia', 'niewiele szczęścia', 'dużo szczęścia'])
-    plt.show()
+    filename = PREDICTED_EMOTION + '_' + name + '_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+    loaded_model = pickle.load(open(filename, 'rb'))
+    new_test_pred = loaded_model.predict(test_x)
+
+    print("Test ACC: " + str(accuracy_score(test_y, test_pred)))
+    print(confusion_matrix(test_y, test_pred))
